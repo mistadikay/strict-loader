cd97461782e03cb7c614273bee6a609eea155cb0
mistadikay <koltsov.d@gmail.com>
2015-03-01 02:09:36 +0700

    :hatching_chick:

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # strict-loader
+
+> Adds `use strict;` in the beginning of each source
+
+## Install
+
+```
+$ npm install --save strict-loader
+```
+
+## Usage
+
+### inline
+
+``` javascript
+require('strict!./yourmom.js');
+```
+
+### webpack config
+
+``` javascript
+module.exports = {
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'strict'
+      }
+    ]
+  }
+};
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var OriginalSource = require('webpack/lib/OriginalSource');
+
+module.exports = function(source, sourceMap){
+    var injection = '"use strict";\n\n';
+    var injectedSource = injection + source;
+    var identifier;
+    var origMap;
+
+    if (this.cacheable) {
+        this.cacheable();
+    }
+
+    if (sourceMap) {
+        identifier = this._module.identifier();
+        origMap = new OriginalSource(source, identifier, sourceMap);
+
+        origMap.node().prepend(injection);
+
+        return this.callback(null, injectedSource, origMap.map());
+    }
+
+    return injectedSource;
+};
