9beaec2e6d5d7d22822befbf724318e330edfe84
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2017-03-26 05:15:19 +0900

    Add generateSourceMapFromTokens option
    To generate source map even though input source map is not specified

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 var loaderUtils = require('loader-utils');
 var SourceMap = require('source-map');
+var SourceMapGenerator = SourceMap.SourceMapGenerator;
+var esprima = require('esprima');
 
 var SourceNode = SourceMap.SourceNode;
 var SourceMapConsumer = SourceMap.SourceMapConsumer;
@@ -13,8 +15,31 @@ module.exports = function(source, map) {
         this.cacheable();
     }
 
-    if (map) {
-        var currentRequest = loaderUtils.getCurrentRequest(this);
+    var defaultOptions = { generateSourceMapFromTokens: false };
+    var loaderOptions = Object.assign(defaultOptions, loaderUtils.getOptions(this) || {});
+    var currentRequest = loaderUtils.getCurrentRequest(this);
+
+    if (loaderOptions.generateSourceMapFromTokens) {
+        var output = prefix + source;
+        var tokens = esprima.tokenize(output, { loc: true });
+        var generator = new SourceMapGenerator({ name: currentRequest });
+
+        generator.setSourceContent(currentRequest, source);
+
+        for (var i = 2; i < tokens.length; i++) {
+            var token = tokens[i];
+
+            generator.addMapping({
+                source: currentRequest,
+                original: { line: token.loc.start.line - 2, column: token.loc.start.column },
+                generated: { line: token.loc.start.line, column: token.loc.start.column }
+            });
+        }
+
+        this.callback(null, output, generator.toJSON());
+
+        return;
+    } else if (map) {
         var node = SourceNode.fromStringWithSourceMap(source, new SourceMapConsumer(map));
 
         node.prepend(prefix);
