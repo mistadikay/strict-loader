2065f08e5348e0b62b1f35f4284a8572ef34dfb1
Denis Koltsov <iam@mistadikay.com>
2017-03-26 13:20:13 +0200

    :space_invader: use 2-space indentation

--- index.js
+++ index.js
@@ -8,49 +8,49 @@ var prefix = '\'use strict\';\n\n';
 
 // used https://github.com/webpack/imports-loader/ as an example
 module.exports = function(source, map) {
-    if (this.cacheable) {
-        this.cacheable();
+  if (this.cacheable) {
+    this.cacheable();
+  }
+
+  var SourceMapGenerator = SourceMap.SourceMapGenerator;
+  var defaultOptions = { generateSourceMapFromTokens: false };
+  var loaderOptions = Object.assign(defaultOptions, loaderUtils.getOptions(this) || {});
+  var currentRequest = loaderUtils.getCurrentRequest(this);
+
+  if (loaderOptions.generateSourceMapFromTokens) {
+    var prefixLineLength = 2;
+    var output = prefix + source;
+    var tokens = esprima.tokenize(output, { loc: true });
+    var generator = new SourceMapGenerator({ name: currentRequest });
+
+    generator.setSourceContent(currentRequest, source);
+
+    for (var i = prefixLineLength; i < tokens.length; i++) {
+      var token = tokens[i];
+
+      generator.addMapping({
+        source: currentRequest,
+        original: { line: token.loc.start.line - prefixLineLength, column: token.loc.start.column },
+        generated: { line: token.loc.start.line, column: token.loc.start.column }
+      });
     }
 
-    var SourceMapGenerator = SourceMap.SourceMapGenerator;
-    var defaultOptions = { generateSourceMapFromTokens: false };
-    var loaderOptions = Object.assign(defaultOptions, loaderUtils.getOptions(this) || {});
-    var currentRequest = loaderUtils.getCurrentRequest(this);
+    this.callback(null, output, generator.toJSON());
 
-    if (loaderOptions.generateSourceMapFromTokens) {
-        var prefixLineLength = 2;
-        var output = prefix + source;
-        var tokens = esprima.tokenize(output, { loc: true });
-        var generator = new SourceMapGenerator({ name: currentRequest });
+    return;
+  } else if (map) {
+    var SourceNode = SourceMap.SourceNode;
+    var SourceMapConsumer = SourceMap.SourceMapConsumer;
+    var node = SourceNode.fromStringWithSourceMap(source, new SourceMapConsumer(map));
 
-        generator.setSourceContent(currentRequest, source);
+    node.prepend(prefix);
 
-        for (var i = prefixLineLength; i < tokens.length; i++) {
-            var token = tokens[i];
+    var result = node.toStringWithSourceMap({ file: currentRequest });
 
-            generator.addMapping({
-                source: currentRequest,
-                original: { line: token.loc.start.line - prefixLineLength, column: token.loc.start.column },
-                generated: { line: token.loc.start.line, column: token.loc.start.column }
-            });
-        }
+    this.callback(null, result.code, result.map.toJSON());
 
-        this.callback(null, output, generator.toJSON());
+    return;
+  }
 
-        return;
-    } else if (map) {
-        var SourceNode = SourceMap.SourceNode;
-        var SourceMapConsumer = SourceMap.SourceMapConsumer;
-        var node = SourceNode.fromStringWithSourceMap(source, new SourceMapConsumer(map));
-
-        node.prepend(prefix);
-
-        var result = node.toStringWithSourceMap({ file: currentRequest });
-
-        this.callback(null, result.code, result.map.toJSON());
-
-        return;
-    }
-
-    return prefix + source;
+  return prefix + source;
 };
--- webpack.config.js
+++ webpack.config.js
@@ -5,36 +5,36 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    stats: {
-        colors: true,
-        reasons: false
-    },
-    output: {
-        pathinfo: true,
-        path: path.resolve('./'),
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    devtool: '#source-map',
-    entry: [
-        './demo/index'
-    ],
-    module: {
+  stats: {
+    colors: true,
+    reasons: false
+  },
+  output: {
+    pathinfo: true,
+    path: path.resolve('./'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devtool: '#source-map',
+  entry: [
+    './demo/index'
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
         loaders: [
-            {
-                test: /\.js$/,
-                loaders: [
-                    './index.js?generateSourceMapFromTokens'
-                ]
-            }
+          './index.js?generateSourceMapFromTokens'
         ]
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin(),
-        new HtmlWebpackPlugin({
-            template: 'demo/index.html',
-            assets: { bundle: 'bundle.js' }
-        })
+      }
     ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'demo/index.html',
+      assets: { bundle: 'bundle.js' }
+    })
+  ]
 };
