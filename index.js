5450cb44fe4ff132fc063fb55a0abec90191efb1
Denis Koltsov <iam@mistadikay.com>
2017-03-26 13:17:49 +0200

    :lipstick: stylistic refactoring

--- index.js
+++ index.js
@@ -2,11 +2,8 @@
 
 var loaderUtils = require('loader-utils');
 var SourceMap = require('source-map');
-var SourceMapGenerator = SourceMap.SourceMapGenerator;
 var esprima = require('esprima');
 
-var SourceNode = SourceMap.SourceNode;
-var SourceMapConsumer = SourceMap.SourceMapConsumer;
 var prefix = '\'use strict\';\n\n';
 
 // used https://github.com/webpack/imports-loader/ as an example
@@ -15,23 +12,25 @@ module.exports = function(source, map) {
         this.cacheable();
     }
 
+    var SourceMapGenerator = SourceMap.SourceMapGenerator;
     var defaultOptions = { generateSourceMapFromTokens: false };
     var loaderOptions = Object.assign(defaultOptions, loaderUtils.getOptions(this) || {});
     var currentRequest = loaderUtils.getCurrentRequest(this);
 
     if (loaderOptions.generateSourceMapFromTokens) {
+        var prefixLineLength = 2;
         var output = prefix + source;
         var tokens = esprima.tokenize(output, { loc: true });
         var generator = new SourceMapGenerator({ name: currentRequest });
 
         generator.setSourceContent(currentRequest, source);
 
-        for (var i = 2; i < tokens.length; i++) {
+        for (var i = prefixLineLength; i < tokens.length; i++) {
             var token = tokens[i];
 
             generator.addMapping({
                 source: currentRequest,
-                original: { line: token.loc.start.line - 2, column: token.loc.start.column },
+                original: { line: token.loc.start.line - prefixLineLength, column: token.loc.start.column },
                 generated: { line: token.loc.start.line, column: token.loc.start.column }
             });
         }
@@ -40,13 +39,13 @@ module.exports = function(source, map) {
 
         return;
     } else if (map) {
+        var SourceNode = SourceMap.SourceNode;
+        var SourceMapConsumer = SourceMap.SourceMapConsumer;
         var node = SourceNode.fromStringWithSourceMap(source, new SourceMapConsumer(map));
 
         node.prepend(prefix);
 
-        var result = node.toStringWithSourceMap({
-            file: currentRequest
-        });
+        var result = node.toStringWithSourceMap({ file: currentRequest });
 
         this.callback(null, result.code, result.map.toJSON());
 
